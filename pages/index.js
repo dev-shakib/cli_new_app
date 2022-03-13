20b119c463ade23bb085aadb5dfb5fceb2c93632
dev-shakib <h@lskit.com>
2022-03-13 09:12:22 +0600

    Product Selector with thumnnail

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,42 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-export default function Index() {
+// import React from 'react';
+import React, { useState } from 'react';
+import ProductList from "./components/ProductList";
+
+
+function index() {
+  const [isOpen, setIsOpen] = useState(false);
+  const[products, setProducts] = useState([]);
+  function handleProductSelection(payload){
+    setIsOpen(false);
+    setProducts(payload.selection);
+    console.log(payload);
+  }
   return (
-    <Page>
-      <Heading>
-        Shopify app with Node and React{" "}
-        <span role="img" aria-label="tada emoji">
-          🎉
-        </span>
-      </Heading>
+    <Page
+      title="Product Selector"
+      primaryAction={{
+        content: "Select Product",
+        onAction: () => setIsOpen(true),
+      }}
+    >
+      <p>Page content</p>
+      <ResourcePicker
+      resourceType="Product"
+      open={isOpen}
+      onCancel={ () => setIsOpen(false)}
+      onSelection={handleProductSelection}
+      />
+      {products.map(product => <div>{product.title}, {product.status}</div>)}
+
+     <ProductList products={products} />
     </Page>
   );
 }
+
+export default index;
+
+
+// video no 10 start 
